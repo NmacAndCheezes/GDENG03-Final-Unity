1f7da09af52d88154040f31ded784af732fa083e
NmacAndCheezes <100694636+NmacAndCheezes@users.noreply.github.com>
2024-12-06 14:06:56 +0800

    git

--- GDENG03 FINAL/Assets/SaveToCustomEngineEditor.cs
+++ GDENG03 FINAL/Assets/SaveToCustomEngineEditor.cs
@@ -7,18 +7,20 @@ public class SaveToCustomEngineEditor : Editor
     
     public override void OnInspectorGUI()
     {
+        EditorGUI.BeginChangeCheck();
         SceneSaver t = (SceneSaver)this.target;
+        
         t.saveName = GUILayout.TextArea(t.saveName, 100);
         if (GUILayout.Button("Save"))
         {
             t.Save();
         }
-
         t.path = GUILayout.TextArea(t.path, 100);
         if (GUILayout.Button("Load"))
         {
             t.Load();
         }
+        EditorGUI.EndChangeCheck();
     }
 
 
